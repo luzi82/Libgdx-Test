ccb590920252955316eef81c3a197c00e6509d7b
luzi82 <luzi82@gmail.com>
2013-06-29 16:45:44 +0800

    remove camera

--- LibgdxTest/src/com/luzi82/libgdxtest/TextScreen.java
+++ LibgdxTest/src/com/luzi82/libgdxtest/TextScreen.java
@@ -2,14 +2,12 @@ package com.luzi82.libgdxtest;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL10;
-import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.BitmapFontCache;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class TextScreen extends LTScreen {
 
-	private OrthographicCamera camera;
 	private SpriteBatch batch;
 
 	private BitmapFont font;
@@ -20,13 +18,12 @@ public class TextScreen extends LTScreen {
 		float w = Gdx.graphics.getWidth();
 		float h = Gdx.graphics.getHeight();
 
-		camera = new OrthographicCamera(w, h);
 		batch = new SpriteBatch();
 
 		font = new BitmapFont();
 		fontCache = new BitmapFontCache(font);
 		fontCache.setColor(0, 0, 0, 1);
-		fontCache.setText("HelloWorld", 0, 0);
+		fontCache.setText("HelloWorld", w / 2, h / 2);
 	}
 
 	@Override
@@ -34,7 +31,6 @@ public class TextScreen extends LTScreen {
 		batch.dispose();
 		font.dispose();
 		fontCache.dispose();
-		camera = null;
 		batch = null;
 		font = null;
 		fontCache = null;
@@ -45,7 +41,6 @@ public class TextScreen extends LTScreen {
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
 
-		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
 		fontCache.draw(batch);
 		batch.end();
